960a76bcdbf38837374743946413afdba5ec4167
ViktorSveins <viktorsveins@gmail.com>
2019-10-31 11:09:46 +0000

    comments

--- Objects/Game3DObjects.py
+++ Objects/Game3DObjects.py
@@ -21,9 +21,6 @@ class Brick(Cube):
         self.sides.append(LineObstacle(self.corner_4, self.corner_3))
         self.sides.append(LineObstacle(self.corner_1, self.corner_4))
 
-        self.collision_side = 0
-
-
     def display(self, model_matrix, shader):
         shader.set_mat_diffuse(self.color.r, self.color.g, self.color.b)
         model_matrix.push_matrix()
@@ -36,7 +33,6 @@ class Brick(Cube):
     def collision(self, ball, delta_time):
         # Optimize to only check the sides facing the ball
         sidesToCheck = []
-
         if ball.pos.x < self.pos.x:
             sidesToCheck.append(self.sides[0])
         else:
@@ -47,23 +43,22 @@ class Brick(Cube):
         else:
             sidesToCheck.append(self.sides[1])
 
+        # Check which of the two sides is closer to the ball
+        # This is the case of a corner, so that the ball ricochets of the correct side
         distX = abs(sidesToCheck[0].point_1.x - ball.pos.x)
         distY = abs(sidesToCheck[1].point_1.y - ball.pos.y)
         
+        # Reverse the array if other side is closer
         if distY > distX:
             sidesToCheck.reverse()
         
+        # Check the sides in the order defined above
         for side in sidesToCheck:
             collidedBall = side.collision(ball, delta_time)
             if collidedBall != ball:
                 self.collided = True
                 return collidedBall
         return ball
-
-    def reflection(self, c_motion):
-        return self.sides[self.collision_side].reflection(c_motion)
-
-
 
 class Ball(Sphere):
     def __init__(self, position, size, color=Color(1.0, 0.874, 0.0)):
@@ -135,57 +130,60 @@ class LineObstacle(Line):
     def collision(self, ball, delta_time):
         distX = self.point_1.x - self.point_2.x
         distY = self.point_1.y - self.point_2.y
-        
+
         length = sqrt(distX**2 + distY**2)
         
         dotproduct = ( ((ball.pos.x - self.point_1.x) * (self.point_2.x - self.point_1.x))
                      + ((ball.pos.y - self.point_1.y) * (self.point_2.y - self.point_1.y)) ) / length**2
-
+        
+        # Closest point on line from the balls' center
         closestPointOnLine = Point(self.point_1.x + dotproduct * (self.point_2.x - self.point_1.x), 
                                    self.point_1.y + dotproduct * (self.point_2.y - self.point_1.y), 0)
 
         # Check whether either end of line is inside the ball (check the corners)
         if self.pointInsideCircle(ball, self.point_1):
-            print(f"ball = {ball.pos}")
-            print(f"point_1 = {self.point_1}")
-            print(f"point_2 = {self.point_2}")
-
             return self.redirectBall(ball, self.point_1)
         elif self.pointInsideCircle(ball, self.point_2):
-            print(f"ball = {ball.pos}")            
-            print(f"point_1 = {self.point_2}")
-            print(f"point_2 = {self.point_1}")
             return self.redirectBall(ball, self.point_2)
         
         # Check if the closest point on the line is already inside the ball
         if self.pointInsideCircle(ball, closestPointOnLine) and self.pointInLineRange(closestPointOnLine):
             return self.redirectBall(ball, closestPointOnLine)
 
+        # Find the vector from the balls' center to the closest point on the line
         vector = Vector(ball.pos.x - closestPointOnLine.x, ball.pos.y - closestPointOnLine.y, 0)
 
+        # If the points are the same we don't want to normalize
         if vector.length() == 0:
             closestPointOnCircle = closestPointOnLine
             traversal = Vector(0, 0, 0)
         else:
             vector.normalize()
+            # Traversal is the vector that the ball's center must be moved along to push the ball back out of the line
             traversal = vector * ball.radius
             closestPointOnCircle = Point(ball.pos.x - traversal.x, ball.pos.y - traversal.y, 0)
        
-
+        # Check whether the dotproduct is 0 to avoid division by zero
         normalDot = self.normal_vector.dot(ball.motion)
         if(normalDot == 0):
             t_hit = 0
         else:    
             t_hit = self.normal_vector.dot(self.point_2 - closestPointOnCircle) / normalDot
 
+        # Check whether the hit is in this frame
         if t_hit > 0 and t_hit <= delta_time:
+            # Point of hit
             p_hit = closestPointOnCircle + ball.motion * t_hit
+            # Check if the point of hit is on the line (not just the equation of the line)
             if self.pointInLineRange(p_hit):
+                # Create new ball so other components know that there was a collision
                 newBall = Ball(Point(0,0,0), ball.size, ball.color)
+                # Push ball out of line
                 newBall.pos = p_hit + traversal
+                # Change it's motion vector
                 newBall.motion = self.reflection(ball.motion)
-                print("INSIDE DELTATIME THINGY")
                 return newBall
+        # Return unchanged ball if no collision
         return ball
     
     def reflection(self, c_motion):
